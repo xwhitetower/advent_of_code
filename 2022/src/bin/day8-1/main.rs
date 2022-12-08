ca0ca6983e65a4dc2e0d2c27c4835beeada01061
xwhitetower <arrawnt77@gmail.com>
2022-12-08 19:42:19 +0000

    day 8 solutions

---
+++ 2022/src/bin/day8-1/main.rs
@@ -0,0 +1,68 @@
+use std::cmp::max;
+use std::error::Error;
+use std::time::Instant;
+
+use advent_of_code::read_lines;
+
+fn solve(lines: Vec<String>) -> u32 {
+    let trees: Vec<Vec<char>> = lines.iter().map(|x| x.chars().collect()).collect();
+    let visible_trees = mark_tree_visibility(trees);
+    visible_trees.iter().map(|x| x.iter().filter(|x| **x).count() as u32 ).sum()
+}
+
+fn mark_tree_visibility(trees: Vec<Vec<char>>) -> Vec<Vec<bool>> {
+    let mut visible_trees = vec![vec![false; trees[0].len()]; trees.len()];
+
+    let row_count = trees.len();
+    let column_count = trees[0].len();
+    // This is ugly and repeated. Not enought time to DRY it
+    for i in 0..row_count {
+        let mut min_height = ('0' as u8 - 1) as char;
+        for j in 0..column_count {
+            let tree = trees[i][j];
+            visible_trees[i][j] = visible_trees[i][j] || tree > min_height;
+            min_height = max(min_height, tree)
+        }
+    }
+    for i in 0..row_count {
+        let mut min_height = ('0' as u8 - 1) as char;
+        for j in (0..column_count).rev() {
+            let tree = trees[i][j];
+            visible_trees[i][j] = visible_trees[i][j] || tree > min_height;
+            min_height = max(min_height, tree)
+        }
+    }
+    for j in 0..column_count {
+        let mut min_height = ('0' as u8 - 1) as char;
+        for i in 0..row_count {
+            let tree = trees[i][j];
+            visible_trees[i][j] = visible_trees[i][j] || tree > min_height;
+            min_height = max(min_height, tree)
+        }
+    }
+    for j in 0..column_count {
+        let mut min_height = ('0' as u8 - 1) as char;
+        for i in (0..row_count).rev() {
+            let tree = trees[i][j];
+            visible_trees[i][j] = visible_trees[i][j] || tree > min_height;
+            min_height = max(min_height, tree)
+        }
+    }
+
+    visible_trees
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let timer = Instant::now();
+    println!("{}", solve(read_lines("./data/day8/input.txt")?));
+    println!("total time [{:.2?}]", timer.elapsed());
+    Ok(())
+}
+
+#[test]
+fn test() {
+    let lines = read_lines("./data/day8/test_input.txt").unwrap();
+    let expected_lines = read_lines("./data/day8/part1/test_result.txt").unwrap();
+    let expect = expected_lines.first().unwrap().parse::<u32>().unwrap();
+    assert_eq!(solve(lines), expect);
+}
---
+++ 2022/src/bin/day8-2/main.rs
@@ -0,0 +1,64 @@
+use std::cmp::max;
+use std::error::Error;
+use std::time::Instant;
+
+use advent_of_code::read_lines;
+
+fn solve(lines: Vec<String>) -> u32 {
+    let trees: Vec<Vec<char>> = lines.iter().map(|x| x.chars().collect()).collect();
+    highest_scenic_score(&trees)
+}
+
+fn highest_scenic_score(trees: &Vec<Vec<char>>) -> u32 {
+    let mut max_score: u32 = 0;
+
+    let row_count = trees.len();
+    let column_count = trees[0].len();
+    let test = tree_scenic_score(trees, 3, 2);
+    for i in 0..row_count {
+        for j in 0..column_count {
+            max_score = max(max_score, tree_scenic_score(trees, i, j));
+        }
+    }
+    max_score
+}
+
+fn tree_scenic_score(trees: &Vec<Vec<char>>, row: usize, column: usize) -> u32 {
+    let tree = trees[row][column];
+    let row_access = |i: usize| -> char { trees[i][column] };
+    let colmun_access = |i: usize| -> char { trees[row][i] };
+    viewing_distance(tree, (0..row).rev(), &row_access )
+        * viewing_distance(tree, row + 1..trees.len(), &row_access )
+        * viewing_distance(tree, (0..column).rev(), &colmun_access )
+        * viewing_distance(tree, column + 1..trees[0].len(), &colmun_access )
+}
+
+fn viewing_distance<T: Iterator<Item = usize>>(
+    tree: char,
+    iterator: T,
+    tree_accessor: &dyn Fn(usize) -> char
+) -> u32 {
+    let mut distance = 0;
+    for i in iterator {
+        distance  += 1;
+        if tree_accessor(i) >= tree {
+            break;
+        }
+    }
+    distance
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let timer = Instant::now();
+    println!("{}", solve(read_lines("./data/day8/input.txt")?));
+    println!("total time [{:.2?}]", timer.elapsed());
+    Ok(())
+}
+
+#[test]
+fn test() {
+    let lines = read_lines("./data/day8/test_input.txt").unwrap();
+    let expected_lines = read_lines("./data/day8/part2/test_result.txt").unwrap();
+    let expect = expected_lines.first().unwrap().parse::<u32>().unwrap();
+    assert_eq!(solve(lines), expect);
+}
