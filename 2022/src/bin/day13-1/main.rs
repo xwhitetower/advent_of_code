81c3ec88874dd5ebf2fda05d455129f8efe29756
xwhitetower <arrawnt77@gmail.com>
2022-12-13 19:04:47 +0000

    day 13-1 improved solutions

--- 2022/src/bin/day13-1/main.rs
+++ 2022/src/bin/day13-1/main.rs
@@ -1,4 +1,4 @@
- use std::cmp::min;
+use std::cmp::min;
 use std::error::Error;
 use std::time::Instant;
 use std::cmp::Ordering;
@@ -10,12 +10,50 @@ enum Signal {
     Vector(Vec<Signal>)
 }
 
+impl Ord for Signal {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self {
+            Signal::Integer(raw_left) => {
+                match other {
+                    Signal::Integer(raw_right) => raw_left.cmp(raw_right),
+                    Signal::Vector(_) => Signal::Vector(vec![Signal::Integer(*raw_left)]).cmp(&self)
+                }
+            }
+            Signal::Vector(raw_left) => {
+                match other {
+                    Signal::Integer(raw_right) => self.cmp(&Signal::Vector(vec![Signal::Integer(*raw_right)])),
+                    Signal::Vector(raw_right) => {
+                        for i in 0..min(raw_left.len(), raw_right.len()) {
+                            let order = raw_left[i].cmp(&raw_right[i]);
+                            if order != Ordering::Equal { return order }
+                        }
+                        raw_left.len().cmp(&raw_right.len())
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl PartialOrd for Signal {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Signal {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other) == Ordering::Equal
+    }
+}
+impl Eq for Signal {}
+
 fn solve(lines: Vec<String>) -> u32 {
     let mut solution = 0;
     lines.chunks(3).enumerate().for_each(|(i, packets)| {
         let left = parse_packet(&packets[0]);
         let right = parse_packet(&packets[1]);
-        if cmp(&left, &right) == Ordering::Less {
+        if left < right {
             solution += 1 + i as u32;
         }
     });
@@ -23,15 +61,15 @@ fn solve(lines: Vec<String>) -> u32 {
 }
 
 fn parse_packet(packet: &String) -> Signal {
-    internal_parsing(packet).0
+    internal_parsing(&packet.as_str()).0
 }
 
-fn internal_parsing(packet: &String) -> (Signal, usize) {
+fn internal_parsing(packet: &str) -> (Signal, usize) {
     if packet.chars().next().unwrap() == '[' {
         let mut next_position = 1;
         let mut vector_signal = Vec::new();
         while get_first_char(&packet[next_position..]) != ']' {
-            let (signal, end_position) = internal_parsing(&packet[next_position..].to_string());
+            let (signal, end_position) = internal_parsing(&packet[next_position..]);
             next_position += end_position;
             if get_first_char(&packet[next_position..]) == ',' { next_position += 1 };
             vector_signal.push(signal);
@@ -47,31 +85,8 @@ fn get_first_char(str: &str) -> char {
     str.chars().next().unwrap_or(']')
 }
 
-fn get_integer_end(packet: &String) -> usize {
+fn get_integer_end(packet: &str) -> usize {
     packet.find(|c| c == ',' || c == ']').unwrap_or(packet.len())
-}
-
-fn cmp(left: &Signal, right: &Signal) -> Ordering {
-    match left {
-        Signal::Integer(raw_left) => {
-            match right {
-                Signal::Integer(raw_right) => raw_left.cmp(raw_right),
-                Signal::Vector(_) => cmp(&Signal::Vector(vec![Signal::Integer(*raw_left)]), &right)
-            }
-        }
-        Signal::Vector(raw_left) => {
-            match right {
-                Signal::Integer(raw_right) => cmp(&left, &Signal::Vector(vec![Signal::Integer(*raw_right)])),
-                Signal::Vector(raw_right) => {
-                    for i in 0..min(raw_left.len(), raw_right.len()) {
-                        let order = cmp(&raw_left[i], &raw_right[i]);
-                        if order != Ordering::Equal { return order }
-                    }
-                    raw_left.len().cmp(&raw_right.len())
-                }
-            }
-        }
-    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
