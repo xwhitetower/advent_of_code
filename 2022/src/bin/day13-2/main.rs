cb918573506cd511e6034f3899a1ae91c742844c
xwhitetower <arrawnt77@gmail.com>
2022-12-13 14:22:41 +0000

    day 13-2 improved solutions

--- 2022/src/bin/day13-2/main.rs
+++ 2022/src/bin/day13-2/main.rs
@@ -1,100 +1,31 @@
-use std::cmp::min;
 use std::error::Error;
 use std::time::Instant;
-use std::cmp::Ordering;
 
 use advent_of_code::read_lines;
 
-#[derive(Debug)]
-enum Signal {
-    Integer(i32),
-    Vector(Vec<Signal>)
-}
-
-impl Signal {
-    fn is_divider_signal(self: &Self) -> bool {
-        match self {
-            Signal::Vector(raw_0) => {
-                if raw_0.len() != 1 { return false }
-                match &raw_0[0] {
-                    Signal::Vector(raw_1) => {
-                        if raw_1.len() != 1 { return false }
-                         match raw_1[0] {
-                             Signal::Integer(raw_2) => raw_2 == 2 || raw_2 == 6,
-                             _ => false
-                         }
-                    },
-                    _ => false
-                }
-            },
-            _ => false
-        }
-    }
-}
-
-fn solve(mut lines: Vec<String>) -> u32 {
-    lines.push("[[2]]".to_string());
-    lines.push("[[6]]".to_string());
-    let mut packets: Vec<Signal> = lines.iter()
+fn solve(lines: Vec<String>) -> u32 {
+    let mut smaller_than_2 = 0;
+    let mut smaller_than_6 = 0;
+    lines.iter()
         .filter(|x| !x.is_empty())
-        .map(|x| parse_packet(x))
-        .collect();
-    packets.sort_by(|a, b| cmp(&a, &b));
-    packets.iter().enumerate()
-        .filter(|(_, s)| s.is_divider_signal())
-        .fold(1, |acc, (i, _)| acc * (i + 1) as u32 )
-}
-
-fn parse_packet(packet: &String) -> Signal {
-    internal_parsing(packet).0
-}
-
-fn internal_parsing(packet: &String) -> (Signal, usize) {
-    if packet.chars().next().unwrap() == '[' {
-        let mut next_position = 1;
-        let mut vector_signal = Vec::new();
-        while get_first_char(&packet[next_position..]) != ']' {
-            let (signal, end_position) = internal_parsing(&packet[next_position..].to_string());
-            next_position += end_position;
-            if get_first_char(&packet[next_position..]) == ',' { next_position += 1 };
-            vector_signal.push(signal);
-        }
-        (Signal::Vector(vector_signal), next_position + 1)
-    } else {
-        let end = get_integer_end(packet);
-        (Signal::Integer(packet[..end].parse().unwrap()), end)
-    }
-}
-
-fn get_first_char(str: &str) -> char {
-    str.chars().next().unwrap_or(']')
-}
-
-fn get_integer_end(packet: &String) -> usize {
-    packet.find(|c| c == ',' || c == ']').unwrap_or(packet.len())
-}
-
-fn cmp(left: &Signal, right: &Signal) -> Ordering {
-    match left {
-        Signal::Integer(raw_left) => {
-            match right {
-                Signal::Integer(raw_right) => raw_left.cmp(raw_right),
-                Signal::Vector(_) => cmp(&Signal::Vector(vec![Signal::Integer(*raw_left)]), &right)
-            }
-        }
-        Signal::Vector(raw_left) => {
-            match right {
-                Signal::Integer(raw_right) => cmp(&left, &Signal::Vector(vec![Signal::Integer(*raw_right)])),
-                Signal::Vector(raw_right) => {
-                    for i in 0..min(raw_left.len(), raw_right.len()) {
-                        let order = cmp(&raw_left[i], &raw_right[i]);
-                        if order != Ordering::Equal { return order }
-                    }
-                    raw_left.len().cmp(&raw_right.len())
+        .for_each(|line| {
+            let digit_pos = line.find(|c| c >= '0' && c <= '9');
+            let close_pos = line.find(']');
+            if digit_pos == None || (close_pos != None && close_pos.unwrap() < digit_pos.unwrap()) {
+                smaller_than_2 += 1;
+                smaller_than_6 += 1;
+            } else {
+                let pos = digit_pos.unwrap();
+                if (close_pos == None || pos < close_pos.unwrap()) && (pos == line.len() - 1
+                        || !(line[pos+1..].chars().next().unwrap() >= '0' && line[pos+1..].chars().next().unwrap() <= '9')
+                    ) {
+                    let current_char = line[pos..].chars().next().unwrap();
+                    if current_char < '2' { smaller_than_2 += 1 }
+                    if current_char < '6' { smaller_than_6 += 1 }
                 }
             }
-        }
-    }
+        });
+    (smaller_than_2 + 1) * (smaller_than_6 + 2)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
