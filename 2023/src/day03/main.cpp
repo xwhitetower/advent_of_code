40229d72f9ab8b7e3a762ea161a7110ab1d33f81
xwhitetower <arrawnt77@gmail.com>
2023-12-23 19:50:44 +0000

    day03 refactor

--- 2023/src/day03/main.cpp
+++ 2023/src/day03/main.cpp
@@ -16,6 +16,30 @@ size_t parse_number(const std::vector<std::string> &schematic, const int x, cons
     return number;
 }
 
+size_t part1(const ElvenIO::input_type &schematic) {
+    size_t sum = 0;
+    for (int y = 0; y < schematic.size(); ++y) {
+        for (int x = 0; x < schematic[y].size(); ++x) {
+            if (schematic[y][x] != EMPTY && !isdigit(schematic[y][x])) {
+                for (int y_i = std::max(y-1, 0); y_i <= y+1 && y_i < schematic.size(); y_i++) {
+                    for (int x_i = std::max(x-1, 0); x_i <= x+1 && x_i < schematic[y_i].size(); x_i++) {
+                        if (isdigit(schematic[y_i][x_i]) &&
+                            (
+                                y == y_i // no extra checks on horizontal
+                                || x_i < x // no extra checks on the left corners
+                                || !isdigit(schematic[y_i][x_i-1]) // not already processed
+                            )
+                        ) {
+                            sum += parse_number(schematic,x_i, y_i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return sum;
+}
+
 size_t gear_ratio(const std::vector<std::string> &schematic, const int x, const int y) {
     int found = 0;
     size_t ratio = 1;
@@ -36,7 +60,7 @@ size_t gear_ratio(const std::vector<std::string> &schematic, const int x, const 
     return found == 2 ? ratio : 0;
 }
 
-size_t solve(const ElvenIO::input_type &schematic) {
+size_t part2(const ElvenIO::input_type &schematic) {
     size_t sum = 0;
     for (int y = 0; y < schematic.size(); ++y) {
         for (int x = 0; x < schematic[y].size(); ++x) {
@@ -49,8 +73,13 @@ size_t solve(const ElvenIO::input_type &schematic) {
 }
 
 int main(int _, char** argv) {
+    ElvenMeasure::Reporter reporter;
     const auto [input, io_time] = ElvenMeasure::execute([=]{ return ElvenIO::read(argv[1]); });
-    auto [result, solution_time] = ElvenMeasure::execute([=] { return solve(input); }, 100);
-    ElvenMeasure::report(result, io_time, solution_time);
+    reporter.add_io_report(io_time);
+    auto [result1, solution1_time] = ElvenMeasure::execute([=] { return part1(input); }, 10);
+    reporter.add_report(1, result1, solution1_time);
+    auto [result2, solution2_time] = ElvenMeasure::execute([=] { return part2(input); }, 10);
+    reporter.add_report(2, result2, solution2_time);
+    reporter.report();
     return 0;
 }
